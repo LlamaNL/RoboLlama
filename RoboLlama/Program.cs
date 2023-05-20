1aba19742f7a80317318a7e5f13234d5921edd5a
Nick Thijssen <nick_aap@hotmail.com>
2023-05-20 20:27:41 +0200

    Made RoboLlama into a windows service

--- RoboLlama/Program.cs
+++ RoboLlama/Program.cs
@@ -3,13 +3,13 @@ using RoboLlama.Models;
 using RoboLlama.Services;
 
 IHostBuilder builder = Host.CreateDefaultBuilder(args);
-
 builder.ConfigureServices((context, services) =>
 {
     IConfiguration config = context.Configuration;
     services.Configure<ServerConfig>(config.GetSection(ServerConfig.Key));
     services.AddSingleton<IPluginService, PluginService>();
     services.AddHostedService<Bot>();
+    services.AddWindowsService(options => options.ServiceName = "RoboLlama");
 });
 
 IHost host = builder.Build();
